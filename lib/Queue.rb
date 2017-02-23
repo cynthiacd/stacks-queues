bd0589759722c4f9a17d88642f1f6683da0139f6
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-02-23 12:16:07 -0800

    implemented the queue class and have all tests working

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,30 @@
 class Queue
+
   def initialize
+    @line = Array.new
   end
 
   def enqueue(element)
+    @line << element
   end
 
   def dequeue
+    @line.shift
   end
 
   def front
+    @line.first
   end
 
   def size
+    @line.length
   end
 
   def empty?
+    @line.empty?
   end
 
   def to_s
+    @line.to_s
   end
 end
